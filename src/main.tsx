3701295894ead6810ccfd4dd117e9ec9c9a6722e
domashnii22 <domashnii.al@yandex.ru>
2024-08-14 11:24:04 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu.tsx';
+import { Cart } from './pages/Cart/Cart.tsx';
+import { Error } from './pages/Error/Error.tsx';
+import { Layout } from './layout/Menu/Menu.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <Error />,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+);
