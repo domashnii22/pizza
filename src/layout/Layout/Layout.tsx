c6c634929a485b8d01eb5f9e5fd8b6519e157e44
domashnii22 <domashnii.al@yandex.ru>
2024-08-14 17:25:43 +0300

    12.5

---
+++ src/layout/Layout/Layout.tsx
@@ -0,0 +1,53 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
+import Button from '../../components/Button/Button';
+import cn from 'classnames';
+
+export function Layout() {
+  return (
+    <div className={styles['layout']}>
+      <div className={styles['sidebar']}>
+        <div className={styles['user']}>
+          <img
+            className={styles['avatar']}
+            src="/avatar.png"
+            alt="Аватар пользователя"
+          ></img>
+          <p className={styles['name']}>Алексей Гуськов</p>
+          <p className={styles['email']}>email@mail.ru</p>
+        </div>
+        <div className={styles['menu']}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              cn(styles['link'], {
+                [styles.active]: isActive,
+              })
+            }
+          >
+            <img src="/menu-icon.svg" alt="Иконка меню"></img>
+            Меню
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              cn(styles['link'], {
+                [styles.active]: isActive,
+              })
+            }
+          >
+            <img src="/cart-icon.svg" alt="Иконка корзины"></img>
+            Корзина
+          </NavLink>
+        </div>
+        <Button className={styles['exit']}>
+          <img src="/exit-icon.svg" alt="Иконка кнопки выхода"></img>
+          Выход
+        </Button>
+      </div>
+      <div className={styles['content']}>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
-import { Layout } from './layout/Menu/Menu.tsx';
+import { Layout } from './layout/Layout/Layout.tsx';
+import { Product } from './pages/Product/Product.tsx';
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: '/cart',
         element: <Cart />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
       },
     ],
   },
