93fa052ccdc6dc4109885b7ef94a8e1e75814ff0
domashnii22 <domashnii.al@yandex.ru>
2024-08-19 17:32:56 +0300

    13

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,36 @@
-import { StrictMode } from 'react';
+import { lazy, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu.tsx';
+import { createBrowserRouter, defer, RouterProvider } from 'react-router-dom';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Layout/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
+import { PREFIX } from './helpers/API.ts';
+import axios from 'axios';
+import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
+import { Login } from './pages/Login/Login.tsx';
+import { Register } from './pages/Register/Register.tsx';
+import { RequireAuth } from './helpers/RequireAuth.tsx';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: (
+      <RequireAuth>
+        <Layout />
+      </RequireAuth>
+    ),
     children: [
       {
         path: '/',
-        element: <Menu />,
+        element: (
+          <Suspense fallback={<>Загрузка...</>}>
+            <Menu />
+          </Suspense>
+        ),
       },
       {
         path: '/cart',
@@ -24,6 +39,33 @@ const router = createBrowserRouter([
       {
         path: '/product/:id',
         element: <Product />,
+        errorElement: <>Ошибка</>,
+        loader: async ({ params }) => {
+          return defer({
+            data: new Promise((resolve, reject) => {
+              setTimeout(() => {
+                axios
+                  .get(`${PREFIX}/products/${params.id}`)
+                  .then(data => resolve(data))
+                  .catch(e => reject(e));
+              }, 2000);
+            }),
+          });
+        },
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
       },
     ],
   },
@@ -38,3 +80,5 @@ createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 );
+
+export default { Menu };
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,94 @@
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Headling from '../../components/Headling/Headling';
+import Input from '../../components/Input/Input';
+import styles from './Login.module.css';
+import { FormEvent, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { PREFIX } from '../../helpers/API';
+import { LoginResponse } from '../../interfaces/auth.interface';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { AppDispath, RootState } from '../../store/store';
+// import { login, userActions } from '../../store/user.slice';
+
+export type LoginForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
+
+export function Login() {
+  const [error, serError] = useState<string | null>();
+  const navigate = useNavigate();
+  //   const dispatch = useDispatch<AppDispath>();
+  //   const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
+
+  //   useEffect(() => {
+  //     if (jwt) {
+  //       navigate('/');
+  //     }
+  //   }, [jwt, navigate]);
+
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    serError(null);
+    // dispatch(userActions.clearLoginError());
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    await sendLogin(email.value, password.value);
+    navigate('/');
+  };
+
+  //   const sendLogin = async (email: string, password: string) => {
+  //     dispatch(login({ email, password }));
+  //   };
+
+  const sendLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+      localStorage.setItem('jwt', data.access_token);
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        console.log(e);
+        serError(e.response?.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className={styles['login']}>
+      <Headling>Вход</Headling>
+      {/* {loginErrorMessage && (
+        <div className={styles['error']}>{loginErrorMessage}</div>
+      )} */}
+      {error && <div className={styles['error']}>{error}</div>}
+      <form className={styles['form']} onSubmit={submit}>
+        <div className={styles['field']}>
+          <label htmlFor="email">Ваш email</label>
+          <Input id="email" name="email" placeholder="Email" />
+        </div>
+        <div className={styles['field']}>
+          <label htmlFor="password">Ваш пароль</label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Пароль"
+          />
+        </div>
+        <Button appearence="big">Вход</Button>
+      </form>
+      <div className={styles['links']}>
+        <div>Нет акканута?</div>
+        <Link to="/auth/register">Зарегистрироваться</Link>
+      </div>
+    </div>
+  );
+}
